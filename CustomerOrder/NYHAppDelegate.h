d0b57fc822e19c7958a1cb074f02a6d60db7140f
ios <ldci@localhost.(none)>
2013-06-05 16:06:47 +0800

    Initial Commit

---
+++ CustomerOrder/NYHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NYHAppDelegate.h
+//  CustomerOrder
+//
+//  Created by ios on 13-6-5.
+//  Copyright (c) 2013年 hxhd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NYHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomerOrder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomerOrder
+//
+//  Created by ios on 13-6-5.
+//  Copyright (c) 2013年 hxhd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NYHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NYHAppDelegate class]));
+    }
+}
